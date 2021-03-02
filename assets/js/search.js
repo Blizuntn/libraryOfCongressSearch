89b290ee02011207b029029282b928941290bfc4
Hagen DeRouen <hagend87@gmail.com>
2021-03-02 14:40:57 -0600

    Add functionality to handle the search form submission.

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,1 +1,24 @@
-// Add search functionality here for index.html
+const searchInputEl = document.getElementById('search');
+const searchTypeEl = document.getElementById('search-type');
+const searchMsgEl = document.getElementById('search-msg');
+
+const handleSearchForm = function() {
+    
+    if (!searchInputEl.value) {
+        displaySearchMsg('Error: please include a search input!');
+        return;
+    }
+
+    const search = searchInputEl.value;
+    const searchType = searchTypeEl.value;
+    const redirectUrl = '../../search-results.html' + 'q=' + search + '&format=' + searchType;
+
+    document.location.replace(redirectUrl);
+}
+
+const displaySearchMsg = function(msg) {
+    searchMsgEl.textContent = msg;
+}
+
+document.addEventListener('submit', handleSearchForm);
+  
